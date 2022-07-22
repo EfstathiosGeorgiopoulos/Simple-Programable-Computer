acd02a87a817ae15aa01dcdae25dfb01af8ed914
EfstathiosGeorgiopoulos <107635932+EfstathiosGeorgiopoulos@users.noreply.github.com>
2022-07-22 15:37:14 +0300

    Update CustomLanguageCompileCodeVer2.cpp

--- ProgramableComputerVer1.2/CustomLanguageCompileCodeVer2.cpp
+++ ProgramableComputerVer1.2/CustomLanguageCompileCodeVer2.cpp
@@ -47,13 +47,13 @@ int data_0[] = {
 int data_1[] = {
   MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0000 - NOP
   MI|CO,  RO|II|CE,  IO|MI,  	RO|AI,  0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0001 - LDA
-  MI|CO,  RO|II|CE,  IO|MI,  	RO|BI,  EO|AI|FI,       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0010 - ADD
+  MI|CO,  RO|II|CE,  IO|MI,  	RO|BI,  EO|AI|FI, 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0010 - ADD
   MI|CO,  RO|II|CE,  IO|MI,  	RO|BI,  EO|AI|SU|FI,    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0011 - SUB
   MI|CO,  RO|II|CE,  IO|MI,  	AO|RI,  0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0100 - STA
   MI|CO,  RO|II|CE,  IO|AI,  	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0101 - LDI
   MI|CO,  RO|II|CE,  IO|J,   	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0110 - JMP
-  MI|CO,  RO|II|CE,  IO|J,    	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0111 - JC
-  MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1000 - JZ
+  MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0111 - JC
+  MI|CO,  RO|II|CE,  IO|J,     	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1000 - JZ
   MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1001
   MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1010
   MI|CO,  RO|II|CE,  EO|SU|OI,  0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1011 - SUBOUT
@@ -66,13 +66,13 @@ int data_1[] = {
 int data_2[] = {
   MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0000 - NOP
   MI|CO,  RO|II|CE,  IO|MI,  	RO|AI,  0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0001 - LDA
-  MI|CO,  RO|II|CE,  IO|MI,  	RO|BI,  EO|AI|FI, 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0010 - ADD
+  MI|CO,  RO|II|CE,  IO|MI,  	RO|BI,  EO|AI|FI,       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0010 - ADD
   MI|CO,  RO|II|CE,  IO|MI,  	RO|BI,  EO|AI|SU|FI,    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0011 - SUB
   MI|CO,  RO|II|CE,  IO|MI,  	AO|RI,  0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0100 - STA
   MI|CO,  RO|II|CE,  IO|AI,  	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0101 - LDI
   MI|CO,  RO|II|CE,  IO|J,   	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0110 - JMP
-  MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0111 - JC
-  MI|CO,  RO|II|CE,  IO|J,     	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1000 - JZ
+  MI|CO,  RO|II|CE,  IO|J,    	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 0111 - JC
+  MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1000 - JZ
   MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1001
   MI|CO,  RO|II|CE,  0,      	0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1010
   MI|CO,  RO|II|CE,  EO|SU|OI,  0,      0,         	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,   // 1011 - SUBOUT
